f04821744a9955145037afec06dd5414cf9d24a8
Umprofeti <vansthewall16@gmail.com>
2024-05-23 13:10:56 -0500

    Se agregan los modelos para el funcionamiento de la web

---
+++ lab3-part2/models/evento.js
@@ -0,0 +1,10 @@
+const evento = [
+    {
+        id: 1, // -> Representa el ID interno no se utiliza
+        usuariosInfo: [
+            {
+                id: 1, // -> Representa el ID 
+            }
+        ] // Arreglo de usuarios al cual se han suscrito al evento contiene su información
+    }
+]
--- lab3-part2/models/eventos.js
+++ lab3-part2/models/eventos.js
@@ -1,8 +1,21 @@
 let eventos = [
     {
-        id: 1,
-        nombre: "Noche gotica en Casco Antiguo",
-        Fecha: "12/12/12",
-        imagen: "url"
-    }
+        id: 1, // -> ID del evento
+        nombre: "Noche gotica en Casco Antiguo", // -> Nombre del evento
+        Fecha: "12/12/12", // -> Fecha que se realiza el evento
+        detalle: "Este evento se realizará en el casco antiguo y tiene una plaza de 50 usuarios",
+        comentarios: [
+            {
+                id:1, // -> Asociado al ID del comentario
+                userId: 1 // -> Asociado con el usuario el cual escribió el comentario
+            }
+        ], 
+        imagen: "url", // -> URL de la imagen procedente de un CDN
+        usuarios: [
+            {
+                id:1 // -> Asociado con el ID del usuario al cual se ha inscrito en el evento
+            }
+        ],
+        plazas: 100, // -> INT Cantidad de plazas disponibles, Cuando las plazas se acaben mostrar agotados en el front
+    },
 ]
